209c0b71767041e501d955823f826710eb90ffc9
vaskopavic <vaskopavic@gmail.com>
2023-04-06 14:57:52 +0200

    Login functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,10 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import ClientOnly from "./components/ClientOnly";
 import ToastProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
   title: "Airbnb",
@@ -15,18 +17,20 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToastProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
